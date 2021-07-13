480994c08d90a5fb86148556a18e5e9ef644940e
diomids2 <73174868+diomids2@users.noreply.github.com>
2021-07-13 00:48:27 -0300

    Add files via upload

---
+++ 01-12-20-Atributos e Metodos/Personagem.cs
@@ -0,0 +1,16 @@
+namespace Atributos_e_Metodos_01_12_20
+{
+    public class Personagem
+    {
+        public string nome = "Snowzin";
+
+        public int idade = 35;
+
+        public string armadura;
+
+        public string Atacar()
+    {
+        return "O Snowzin atacou!";
+    }
+    }
+}
---
+++ 01-12-20-Atributos e Metodos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Atributos_e_Metodos_01_12_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem tony = new Personagem();
+            tony.armadura = "Magic Plate Armor";
+
+            Console.WriteLine(tony.nome);
+
+            Console.WriteLine(tony.armadura);
+
+            Console.WriteLine(tony.Atacar());
+        }
+    }
+}
